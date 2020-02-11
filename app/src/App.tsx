0d8f4d549672e1d621336043df0e544538bad9d8
Sierk Beij <sierk@riseup.net>
2020-02-10 23:50:41 -0800

    Added emotion.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,55 @@
+/** @jsx jsx */
 import './App.css'
 
+import { css, jsx, keyframes } from '@emotion/core'
 import React from 'react'
 
 import logo from './logo.svg'
 
-const App = () => {
+const wrapperCss = css`
+  text-align: center;
+`
+const logoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+const logoCss = css`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${logoSpin} infinite 20s linear;
+  }
+`
+const headerCss = css`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+`
+const linkCss = css`
+  color: #61dafb;
+}
+`
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div css={wrapperCss}>
+      <header css={headerCss}>
+        <img src={logo} css={logoCss} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          css={linkCss}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,12 +1,12 @@
-import './index.css'
-
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const rootElement = document.getElementById("root")
+
+render(<App />, rootElement)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
