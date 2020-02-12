9c558a360108b9d5045321037af152fd42701e4e
sierkje <sierk@riseup.net>
2020-02-11 19:58:33 -0800

    Fixed prettier config.

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,7 +4,7 @@ import { render } from 'react-dom'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
-const rootElement = document.getElementById("root")
+const rootElement = document.getElementById('root')
 
 render(<App />, rootElement)
 
